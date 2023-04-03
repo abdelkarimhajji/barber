4ee40b26f8fa979cbba6feb62e99c8ad15bf4252
Abdelkarim hajji <ahajji@MacBook-Pro-van-Abdelkarim.local>
2023-04-03 01:10:05 +0000

    ff

--- js/main.js
+++ js/main.js
@@ -18,17 +18,53 @@ closse.onclick = function() {
   };
 
 // slide
-
-const myButton = document.getElementById("myButton");
-const myText = document.getElementById("myText");
-
-myButton.addEventListener("click", () => {
-  myText.classList.add("slide-out");
+let comnt1 = "Nullam orci dui, dictum et magna sollicitudin, tempor blandit erat. Maecenas suscipit tellus sit amet augue placerat fringilla a id lacus. Fusce tincidunt in leo lacinia condimentum. Donec placerat, orci vel consequat mattis, sapien lacus pretium mi, sed lacinia dolor nibh non mi."
+let comnt2 = "tempor blandit erat. Maecenas suscipit tellus sit amet augue placerat fringilla a id lacus. Fusce tincidunt in leo lacinia condimentum. Donec placerat, orci vel consequat mattis, sapien lacus pretium mi, sed lacinia dolor nibh non mi."
+let comnt3 = "Maecenas suscipit tellus sit amet augue placerat fringilla a id lacus. Fusce tincidunt in leo lacinia condimentum. Donec placerat, orci vel consequat mattis, sapien lacus pretium mi, sed lacinia dolor nibh non mi."
+function fadeOut() {
+  const circle = document.querySelector('.circle');
+  circle.style.opacity = 0; // set opacity to 0 to start the fade out animation
   setTimeout(() => {
-    myText.innerHTML = "You clicked the button!";
-    myText.classList.remove("slide-out");
-  }, 500);
-});
-
+    circle.style.display = 'none'; // hide the element after the animation completes
+  }, 1000); // wait 1 second (1000 milliseconds) for the animation to complete
+}
+let coment = document.getElementsByClassName("coment")[0]
+let nameComntes = document.getElementsByClassName("nameComntes")[0]
+let img = document.getElementById('img')
+const array = [comnt1, comnt2, comnt3]
+const array2 = ["Cassie Carleton", "Coby Sue", "Stefanie Rashfod"]
+const array3 = ["../imgs/cassie.jpeg","../imgs/stefani.jpeg","../imgs/coby.jpeg"]
+let i = 0;
+let j = 0
+function change(par) {
+  if (par == "left")
+  {
+    if (i >= 2)
+      i = 2
+    else
+      i++;
+    coment.innerHTML = array[i]
+    nameComntes.innerHTML  = array2[i]
+    img.src = array3[i]
+  }
+  if (par == "right")
+  {
+    if (i <= 0)
+    {
+      i = 0
+    }
+    else
+      i--
+    coment.innerHTML = array[i]
+    nameComntes.innerHTML  = array2[i]
+    img.src = array3[i]
+  }
+  // j++
+  // if (j < 3)
+  //   setTimeout(change("left"), 1000)
+  // else
+  //   setTimeout(change("right"), 1000)
+}
+// setTimeout(change("left"), 1000)
 
 // finish slide 
